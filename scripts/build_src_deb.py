18c0f45592a4db71555a520ad9c861c4953b7d6c
John Wason <wason@wasontech.com>
2021-10-21 00:43:22 -0400

    build_src_deb.py

--- scripts/build_src_deb.py
+++ scripts/build_src_deb.py
@@ -23,7 +23,7 @@ def main():
         subprocess.check_call(f"tar xf {orig} --strip-components=1", shell=True, cwd=e_dir)
         shutil.copytree(d.joinpath("debian"),e_dir.joinpath("debian"))
         os.link(orig,work_dir.joinpath(f"{dist}/{orig.name}"))
-        subprocess.check_call("debuild -S --no-sign", shell=True, cwd=work_dir.joinpath(f"{dist}/robotraconteur_debian"))
+        subprocess.check_call("debuild -S --no-sign --no-check-builddeps", shell=True, cwd=work_dir.joinpath(f"{dist}/robotraconteur_debian"))
 
     for d in work_dir.iterdir():
         dist = d.name
